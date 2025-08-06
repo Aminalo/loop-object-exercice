947b723b3e83eaeee994738e59fe6baa91f3abca
Amina <107080579+Aminalo@users.noreply.github.com>
2025-08-06 09:23:08 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// Working with Obejcts and Loops!!!
+
+
+//✅ Goal 1: Loop through the following array and calculate the total score.
+//✅ Follow Up: Print your result to the console using string interpolation for clarity.
+// const games = [
+//   { name: "Game 1", score: 5 },
+//   { name: "Game 2", score: 8 },
+//   { name: "Game 3", score: 2 },
+//   { name: "Game 4", score: 10 }
+// ];
+
+//✅ Goal 2: Create another loop that only calculates total of scores greater than 5
+//✅ Follow Up: Print your result to the console using string interpolation for clarity.
+
+
+
+
+// ✅ Goal 3: Find the game with the highest score.
+//✅ Follow Up: Print your result to the console using string interpolation for clarity.
+
+
+// 🌟🌟 Bonus: Print out EACH game that scored above 'average'
+
+
